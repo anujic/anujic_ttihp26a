541304e4c5a52621f969ee4bf106d2fd6e820604
anujic <anujic@ethz.ch>
2026-03-08 14:44:37 +0100

    [RTL] implement top level module

--- src/top.sv
+++ src/top.sv
@@ -12,9 +12,8 @@
  * Inputs:
  * clk_i   - System clock used for sampling and synchronous logic.
  * rst_ni  - Active-low system reset.
- * en_i    - Enable signal. When HIGH, oscillators and samplers are active.
  * ready_i - Handshake signal from the receiver indicating it is 
- * ready to accept a new random byte.
+ * ready to accept a new random byte. When HIGH, oscillators and samplers are active.
  * * Outputs:
  * byte_o  - 8-bit random data payload.
  * valid_o - Handshake signal indicating byte_o contains valid, 
@@ -38,31 +37,149 @@
 module top (
     input logic clk_i,
     input logic rst_ni,
-    input logic en_i,
     input logic ready_i, // Ready Handshake signal
 
     output logic [7:0] byte_o, // Random Byte
     output logic valid_o // Valid Handshake signal
 );
 
-// TODO: implement the module
+// Watchdog signal
+logic wd_timeout;
 
 // Three different ring oscillators to prevent synchronization with clk
 logic bit_3, bit_5, bit_7, random_oscillator_bit;
 
-ring_oscillator #(DEPTH=3) i_ring_oscillator_3 (
+ring_oscillator #(.DEPTH(3)) i_ring_oscillator_3 (
     .bit_o(bit_3)
 )
-ring_oscillator #(DEPTH=5) i_ring_oscillator_5 (
+ring_oscillator #(.DEPTH(5)) i_ring_oscillator_5 (
     .bit_o(bit_5)
 )
-ring_oscillator #(DEPTH=7) i_ring_oscillator_7 (
+ring_oscillator #(.DEPTH(7)) i_ring_oscillator_7 (
     .bit_o(bit_7)
 )
 
 assign random_oscillator_bit = bit_3 ^ bit_5 ^ bit_7;
 
-// Von Neumann Whitener FSM:
+// Synchronize bit
+logic sync_1, sync_2;
 
+always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+        sync_1 <= 1'b0;
+        sync_2 <= 1'b0;
+    end else if (ready_i) begin
+        sync_1 <= random_oscillator_bit;
+        sync_2 <= sync_1;
+    end
+end
+
+// Bit counter
+logic [2:0] counter_d, counter_q;
+logic counter_en, counter_rst;
+
+assign counter_rst = wd_timeout;
+
+always_comb begin
+    counter_d = counter_q;
+    if (counter_en) begin
+        counter_d = counter_q + 1;
+    end
+    if (counter_rst) begin
+        counter_d = 1'b0;
+    end
+end
+
+always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+        counter_q <= '0;
+    end else begin
+        counter_q <= counter_d;
+    end
+end
+
+
+// Von Neumann Whitener FSM:
+typedef enum logic { 
+    FIRST_BIT=0,
+    SECOND_BIT=1
+ } state_e;
+
+ state_e state_d, state_q;
+ logic first_bit_d, first_bit_q;
+ logic clean_bit_d, clean_bit_q;
+ logic valid_bit;
+
+always_comb begin 
+    state_d = state_q;
+    valid_o = 1'b0;
+    first_bit_d = first_bit_q;
+    counter_en = 1'b0;
+    clean_bit_d = clean_bit_q;
+    valid_bit = 1'b0;
+
+    case (state_q)
+        FIRST_BIT: begin
+            if(ready_i) begin
+                first_bit_d = sync_2;
+                state_d = SECOND_BIT;
+            end
+        end
+        SECOND_BIT: begin
+            if(ready_i) begin
+                if (sync_2 ^ first_bit_q) begin
+                    clean_bit_d = first_bit_q ^ sync_2;
+                    counter_en = 1'b1;
+                    valid_bit = 1'b1;
+                end
+                state_d = FIRST_BIT;
+            end
+            
+        end
+        default: begin
+            state_d = FIRST_BIT;
+        end
+    endcase
+
+    if(wd_timeout) begin
+        state_d = FIRST_BIT;
+    end
+end
+
+always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+        state_q <= FIRST_BIT;
+        first_bit_q <= 1'b0;
+    end else begin
+        state_q <= state_d;
+        first_bit_q <= first_bit_d;
+    end
+ end
+
+
+// Byte assembler
+logic [7:0] byte_q, byte_d;
+
+genvar i;
+generate 
+    for (i = 0; i < 8 ;i++ ) begin
+        byte_d = byte_q;
+        if (i == counter_q) begin
+            byte_d[i] = clean_bit_q;
+        end
+    end
+endgenerate
+
+// Watchdog module
+
+watchdog_timer #(.TIMEOUT(1024)) i_watchdog (
+    .clk_i(clk_i),
+    .rst_ni(rst_ni), 
+    .en_i(ready_i),
+    .pet_i(valid_bit),
+    .timeout_o(wd_timeout)
+)
+
+assign valid_o = (counter_q == 7);
 
 endmodule
--- src/watchdog_timer.sv
+++ src/watchdog_timer.sv
@@ -12,18 +12,46 @@
  * Inputs:
  * - clk_i   : System clock
  * - rst_ni  : Active-low system reset
+ * - pet_i   : Resets the Watchdog counter
  * - en_i    : Enables the watchdog counter. When LOW, the counter is paused.
  * * Outputs:
- * - abort_o : Triggers HIGH for one clock cycle when the timeout is reached, 
+ * - timeout_o : Triggers HIGH for one clock cycle when the timeout is reached, 
  * indicating a stall in the entropy generation pipeline.
  ******************************************************************************/
 
 module watchdog_timer #(parameter TIMEOUT = 1024) (
     input logic clk_i,
     input logic rst_ni, 
-    input logic en_i, // enables the watchdog
+    input logic en_i,  // enables the watchdog
+    input logic pet_i, // resets the watchdog counter (the "pet")
 
-    output logic abort_o // is triggered once timeout is reached
+    output logic timeout_o // is triggered for one cycle once timeout is reached
 );
-// TODO: implement module
+
+    logic [$clog2(TIMEOUT)-1 : 0] timer_d, timer_q;
+
+    always_comb begin
+        timer_d   = timer_q;
+        timeout_o = 1'b0;
+
+        if (en_i) begin
+            if (pet_i) begin
+                timer_d = '0; 
+            end else if (timer_q >= (TIMEOUT - 1)) begin
+                timeout_o = 1'b1;
+                timer_d   = '0; 
+            end else begin
+                timer_d = timer_q + 1'b1;
+            end
+        end        
+    end
+
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni) begin
+            timer_q <= '0;
+        end else begin
+            timer_q <= timer_d;
+        end   
+    end
+
 endmodule
