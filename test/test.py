ef8e5e13eb8d2a101b5a722be4a9c0d0d94e6000
anujic <anujic@ethz.ch>
2026-03-08 15:22:31 +0100

    [DV] fix typo in tb

--- test/test.py
+++ test/test.py
@@ -27,7 +27,7 @@ async def test_project(dut):
 
     # Set the input values you want to test
     dut.ui_in.value = 1 # Assert ready_i signal
-    await RisingEdge(uio_out[0]) # Wait until valid_o signal is high
+    await RisingEdge(dut.uio_out[0]) # Wait until valid_o signal is high
 
     random_val = dut.uo_out.value
     dut._log.info(f"The random byte that was generated is: {random_val:x}")
